d4e07046ed25a52596dcc2d45c06515b65cb7c39
heo9999 <heo9999@gmail.com>
2025-05-07 14:07:24 +0900

    init

---
+++ aone-web-svr/src/main/java/com/aone/web/AoneWebSvrApplication.java
@@ -0,0 +1,13 @@
+package com.aone.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoneWebSvrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AoneWebSvrApplication.class, args);
+	}
+
+}
---
+++ aone-web-svr/src/test/java/com/aone/web/AoneWebSvrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aone.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AoneWebSvrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
